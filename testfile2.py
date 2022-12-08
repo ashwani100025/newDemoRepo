44b05c2defda92244429201be10ac643e4faf270
Ashwani Kumar <119838337+ashwani100025@users.noreply.github.com>
2022-12-08 12:58:51 +0530

    Update testfile2.py

--- testfile2.py
+++ testfile2.py
@@ -2,8 +2,6 @@ print("Hello World 7th Dec 9:22 PM")
 print("Ashwani from local")
 print("Line 3 from Local")
 print("Line 3 from GitHub")
-<<<<<<< HEAD
-print("This line is average")
-=======
 print("This line is better")
->>>>>>> 92c3c6fdf6084790f2e39d2722ba68c3ee5516a5
+print("Line 6 from GitHub")
+
